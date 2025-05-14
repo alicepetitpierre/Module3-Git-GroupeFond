0c010d82f8366c7c8543daca1019246ee5a7b30e
bmwi3 <julien.giner@utbm.fr>
2025-05-14 16:24:34 -0400

    Le J test sa branche

--- test.py
+++ test.py
@@ -1,3 +1,22 @@
 print("test")
 print('Hola')
 print('coucuo')
+
+def integral(a, b, n):
+    """
+    Calcule l'intégrale de la fonction f(x) = x^2 sur l'intervalle [a, b] en utilisant la méthode des trapèzes.
+    
+    :param a: Limite inférieure de l'intervalle
+    :param b: Limite supérieure de l'intervalle
+    :param n: Nombre de subdivisions
+    :return: Valeur approchée de l'intégrale
+    """
+    h = (b - a) / n
+    integral = 0.5 * (f(a) + f(b))
+    
+    for i in range(1, n):
+        x = a + i * h
+        integral += f(x)
+    
+    integral *= h
+    return integral
